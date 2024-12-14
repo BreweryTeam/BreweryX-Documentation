ec0e3e79e8a4671c5046d27489e37fa0fe7459db
Nadwey <nadweyyy@gmail.com>
2024-12-14 15:26:36 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,60 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        starlight({
+            title: "BreweryX",
+            logo: {
+                src: "./src/assets/img/breweryx.png",
+                replacesTitle: true,
+            },
+            customCss: ["./src/styles/globals.css", "./src/styles/docs.css"],
+            social: {
+                github: "https://github.com/withastro/starlight",
+            },
+            sidebar: [
+                {
+                    label: "Welcome",
+                    slug: "docs/welcome",
+                },
+                {
+                    label: "Plugin Usage",
+                    items: [
+                        {
+                            label: "Config",
+                            autogenerate: { directory: "docs/plugin-usage/config/" },
+                            collapsed: true,
+                        },
+                    ],
+                },
+                {
+                    label: "For Developers",
+                    items: [
+                        {
+                            label: "Plugin API",
+                            link: "docs/for-developers/plugin-api",
+                        },
+                        {
+                            label: "Addons",
+                            collapsed: true,
+                            autogenerate: { directory: "docs/for-developers/addons/", collapsed: true },
+                        },
+                    ],
+                },
+            ],
+            expressiveCode: {
+                themes: ["slack-dark", "light-plus"]
+            }
+        }),
+        react(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+    ],
+});
---
+++ src/content/docs/docs/plugin-usage/config/creating-a-recipe.md
@@ -0,0 +1,5 @@
+---
+title: "Creating a recipe"
+---
+
+Creating a recipe is somewhat easy with BreweryX. All you need is to follow this guide!
