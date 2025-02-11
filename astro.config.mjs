cc8d27d0073e988568ea3b52040f5d6c0626bf2c
Nadwey <nadweyyy@gmail.com>
2025-02-11 21:42:33 +0100

    refactor: Ugprade to Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import starlight from "@astrojs/starlight";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()]
+    },
     integrations: [
         starlight({
             title: "BreweryX",
@@ -58,8 +61,5 @@ export default defineConfig({
             },
         }),
         react(),
-        tailwind({
-            applyBaseStyles: false,
-        }),
     ],
 });
