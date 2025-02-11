e157311409a62411a71fd75b3df14e273eca9dab
Nadwey <nadweyyy@gmail.com>
2025-02-11 22:58:32 +0100

    feat: Team section (not done yet)

--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -1,8 +1,55 @@
-export default {
-    download: {
+namespace meta {
+    export const download = {
         modrinrth: "https://modrinth.com/plugin/breweryx",
         hangar: "https://hangar.papermc.io/Jsinco/BreweryX",
         spigot: "https://www.spigotmc.org/resources/breweryx.114777/",
-    },
-    discord: "https://discord.gg/3FkNaNDnta",
-};
+    };
+
+    export const media = {
+        discord: "https://discord.gg/3FkNaNDnta",
+    };
+
+    export const team: {
+        name: string;
+        description: string;
+        image: string;
+    }[] = [
+        {
+            name: "Nadwey",
+            description: "If I leave here tomorrow",
+            image: "https://github.com/Nadwey.png",
+        },
+        {
+            name: "Jsinco",
+            description: "Would you still remember me?",
+            image: "https://github.com/Jsinco.png",
+        },
+        {
+            name: "Mitality",
+            description: "For I must be traveling on, now",
+            image: "https://github.com/Mitality.png",
+        },
+        {
+            name: "szarkan",
+            description: "'Cause there's too many places I've got to see",
+            image: "https://github.com/szarkans.png",
+        },
+        {
+            name: "Thorinwasher",
+            description: "But if I stay here with you, girl",
+            image: "https://cdn.discordapp.com/avatars/270177852157067264/99ae3852e6956e3725b175e2b12d690b.webp",
+        },
+        {
+            name: "Tisawesomeness",
+            description: "Things just couldn't be the same",
+            image: "https://github.com/Tisawesomeness.png",
+        },
+        {
+            name: "Aktuv",
+            description: "'Cause I'm as free as a bird now",
+            image: "https://github.com/Vutka1.png",
+        },
+    ];
+}
+
+export default meta;
