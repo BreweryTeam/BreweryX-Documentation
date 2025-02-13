555603a65fe173b66ae29d2ee37ffa468cfe78e2
Thorinwasher <ragnarlol96@gmail.com>
2025-02-13 12:51:23 +0100

    fix(recipegenerator): Ingredients section

--- src/script/ingredients_section.ts
+++ src/script/ingredients_section.ts
@@ -15,8 +15,13 @@ function generateIngredient(): HTMLElement {
     let numberInput = document.createElement("input")
     numberInput.type = "number"
     numberInput.classList.add("bg-gray-200")
+    let clearButton = document.createElement("button")
+    clearButton.textContent = "x"
+    clearButton.classList.add("bg-red-500", "size-8")
+    clearButton.addEventListener("click", () => output.remove())
     output.appendChild(textinput)
     output.appendChild(numberInput)
+    output.appendChild(clearButton)
     output.classList.add("flex", "justify-between", "w-full");
     return output
 }
